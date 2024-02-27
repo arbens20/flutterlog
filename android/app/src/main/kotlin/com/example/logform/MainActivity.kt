cc116b9e42305f7939d61a5c30a06c96743639c6
arbens20 <arbensvital@gmail.com>
2024-02-27 13:21:25 -0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/logform/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.logform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LoginForm());
+}
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('SENTAKS_HT' ,
+          style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.indigo, 
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                children: <Widget>[
+                  Checkbox(
+                    value: _isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        _isChecked = value!;
+                      });
+                    },
+                  ),
+                  Text('Remember me'),
+                ],
+              ),
+              SizedBox(height: 20.0),
+             ElevatedButton(
+                onPressed: () {
+                  String email = _emailController.text;
+                  String password = _passwordController.text;
+                
+                  print('Email: $email, Password: $password');
+
+
+                  
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.indigo, 
+                ),
+                child: Text('Submit',
+                style: TextStyle(color: Colors.white),),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+}
